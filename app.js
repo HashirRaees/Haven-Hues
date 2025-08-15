8e39067917ad18e6750923701c805c76dd9c872b
HashirRaees <hashirrais13@gmail.com>
2025-08-15 16:30:59 +0500

    changed the whole ui

---
+++ app.js
@@ -0,0 +1,62 @@
+var tl = gsap.timeline();
+
+tl.from(".hero h1",{
+    y:20,
+    duration: 0.7,
+    opacity: 0,
+});
+tl.from(".hero span",{
+    duration : 1,
+    opacity: 0,
+})
+tl.from(".hero h2",{
+    y:-10,
+    opacity : 0,
+    duration :.5,
+});
+gsap.from(".gallery img",{
+    opacity:0,
+    y:10,
+    duration:1,
+    scrollTrigger : {
+        scroller: "body",
+        trigger : ".gallery img",
+        // markers : true,
+        start: "top 60%",
+    }
+});
+
+gsap.from("#about h2",{
+    y : 20,
+    duration : 1,
+    opacity : 0,
+    scrollTrigger : {
+        trigger : "#about h2",
+        scroller : 'body',
+        start : "top 80%",
+        // markers : true,
+    }
+})
+gsap.from("#about p",{
+    opacity : 0,
+    duration : 1,
+    scrollTrigger : {
+        trigger : "#about p",
+        scroller : 'body',
+        start : "top 60%",
+        // markers : true,
+    }
+})
+const track = document.querySelector(".marquee-track");
+const trackWidth = track.scrollWidth / 2; // half because we duplicated
+
+gsap.to(track, {
+  x: -trackWidth,
+  duration: 20, // adjust speed
+  ease: "none",
+  repeat: -1,
+  modifiers: {
+    x: gsap.utils.unitize(x => parseFloat(x) % trackWidth) // loop seamlessly
+  }
+});
+
